b791440d1541155142dd41b02ab28aab43f5e678
Macy Crigler <macy.crigler@kellogg.northwestern.edu>
2024-06-01 17:41:18 +0000

    finished the security class

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
+
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where(["user_id" => session["user_id"]])
+    @current_user = User.find_by([ "id" => session["user_id"]])
   end
 
   def create
     @task = Task.new
     @task["description"] = params["description"]
+    @task["user_id"] = session["user_id"]
     @task.save
     redirect_to "/tasks"
   end
